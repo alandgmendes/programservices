5e580b4338e70d55703f8dbf5a8dcc5d52fb1b55
Alandouglas Godinho Mendes <adgmendes@gmail.com>
2023-05-22 19:54:10 -0300

    new try

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,12 +1,11 @@
 const { join } = require('path');
 
 /**
- * 
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(
-    '/tmp/alandgmendes-programservices/.cache/puppeteer/chrome/chrome/linux-113.0.5672.63/chrome/linux-113.0.5672.63',
+    '.cache/puppeteer/chrome/chrome/linux-113.0.5672.63/chrome/linux-113.0.5672.63',
   ),
 };
